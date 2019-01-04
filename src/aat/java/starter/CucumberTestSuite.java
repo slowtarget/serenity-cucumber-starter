5206862a65e4f4949323c5dbf178d83496801608
slowtarget <slowtarget@users.noreply.github.com>
2019-01-04 23:22:52 +0000

    not working yet...

--- src/aat/java/starter/CucumberTestSuite.java
+++ src/aat/java/starter/CucumberTestSuite.java
@@ -7,7 +7,11 @@ import org.junit.runner.RunWith;
 @RunWith(CucumberWithSerenity.class)
 @CucumberOptions(
         plugin = {"pretty"},
-        features = "src/test/resources/features"
+        features = "src/aat/resources/features"
 )
+
+
+
 public class CucumberTestSuite {
+
 }
